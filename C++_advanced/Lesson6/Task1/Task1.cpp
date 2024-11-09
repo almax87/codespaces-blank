687f34f2d38db9352df327ff2d9a4ea7edf3a113
almax87 <amax87@mail.ru>
2024-11-09 16:19:07 +0300

    Task1_v1

--- C++_advanced/Lesson6/Task1/Task1.cpp
+++ C++_advanced/Lesson6/Task1/Task1.cpp
@@ -1,47 +1,29 @@
 #include <iostream>
 #include <vector>
-#include <fstream>
 #include <algorithm>
 #include <map>
 
 int main()
 {
-    std::ifstream in("in.txt");
-    std::string s{};
+    std::string s{"Hello, world!!"};
     char ch{};
-    if (in.is_open())
+    
+    std::cout << "IN: " << s << std::endl;
+    std::map <char, int> map_count{};
+    for (auto& i : s)
     {
-        while (std::getline(in, s))
-        {
-            std::cout << "IN: " << s << std::endl;
-        }
-            
-        std::map <char, int> map_count{};
-        for (auto& i : s)
-        {
-            map_count.insert(std::make_pair(i, std::count(s.begin(), s.end(), i)));
-        }
-
-        std::multimap<int, char, std::greater<int>> map_count_sort{};
-        for (auto& i : map_count)
-        {
-            map_count_sort.insert(std::make_pair(i.second, i.first));
-        }
-
-        std::cout << "OUT: " << std::endl;
-
-        for (auto&i : map_count_sort)
-        {
-            std::cout << i.second << ": " << i.first << std::endl;
-        }
-
-        in.close();
+        map_count.insert(std::make_pair(i, std::count(s.begin(), s.end(), i)));
     }
-    else
-        {
-            std::cout << "Error";
-            return 1;
-        }
+    std::multimap<int, char, std::greater<int>> map_count_sort{};
+    for (auto& i : map_count)
+    {
+        map_count_sort.insert(std::make_pair(i.second, i.first));
+    }
+    std::cout << "OUT: " << std::endl;
+    for (auto&i : map_count_sort)
+    {
+        std::cout << i.second << ": " << i.first << std::endl;
+    }
 
     return 0;
 }
