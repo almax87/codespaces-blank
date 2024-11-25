a30d27c0de055ed9ad1a2869286842114cb3ad07
almax87 <amax87@mail.ru>
2024-11-25 21:37:05 +0300

    init commit

--- C++_advanced/Coursework/Parser.cpp
+++ C++_advanced/Coursework/Parser.cpp
@@ -3,6 +3,21 @@
 #include <map>
 #include <string>
 #include <vector>
+#include <iterator>
+#include <exception>
+#include <stdexcept>
+
+std::string remove_spaces(std::string& str_)
+{
+    while ((str_.size() > 0)&&((*str_.begin() == ' ') || (str_.at(str_.size() - 1) == ' ')))
+    {
+        if (*str_.begin() == ' ')
+            str_.erase(str_.begin());
+        else
+            str_.erase(str_.size() - 1);
+    }
+    return str_;
+}
 
 class Section
 {
@@ -17,64 +32,132 @@ class Section
 
         void set_value(const std::string& val_name, const std::string& value)
         {
-            value_list.insert(value_list.begin(), std::pair<std::string, U>(val_name, value));
+            for (auto i : value_list)
+            {
+                if (i.first == val_name)
+                    i.second = value;
+                else
+                    continue;
+            }
+            value_list.insert(value_list.end(), std::pair<std::string, std::string>(val_name, value));
         }
 
-        std::string& get_name() const
+        std::string get_name() const
         {
             return section_name;
         }
 
+        std::map <std::string, std::string> get_value_list() const
+        {
+            return value_list;
+        }
 };
 
+template <class T>
+class get
+{
+    public:
 
-class IniParser
+    T get_value(const std::string& input)
+    {
+        if (input.find(".") != std::string::npos)
+        {
+            std::string val_name = input.substr(0, input.find("."));
+            remove_spaces(val_name);
+            std::string val = input.substr(input.find('.') + 1, (input.size() - input.find('.') - 1));
+            val = remove_spaces(val);
+        }
+        else
+        {
+            throw std::runtime_error("Wrong syntax!!!");
+        }
+
+    };
+};
+
+class IniParser : public get<int>
 {
     public:
         std::string filename{};
         std::ifstream file;
-        std::map <int, std::string> table{};
+        std::vector <std::string> table{};
         std::vector <Section> section_list{};
-        
+
+    bool find_index(const std::string& sec_name_, int& i)
+    {
+        for (int i = 0; i < section_list.size() - 1; i++)
+        {
+            if(section_list[i].get_name() == sec_name_)
+                return 1;
+            else
+                return 0;
+        };
+        return 0;
+    }
+
+  void set_section()
+    {
+        std::string val_name;
+        std::string val;
+        int index{};
+        for (auto i = table.begin(); i < table.end(); i++)
+        {
+            if ((*begin(*i) == '[') && (*(end(*i) - 1)) == ']')
+            {
+                i->erase(begin(*i));
+                i->erase(end(*i) - 1);
+                if (section_list.empty())
+                    {
+                        Section section(*i);
+                        section_list.push_back(section);
+                    }
+                else
+                    {
+                        if (find_index(*i, index))
+                        {
+                            break;
+                        }
+                        else
+                        {
+                            Section section(*i);
+                            section_list.push_back(section);
+                            index = section_list.size() - 1;
+                        }
+                    }
+                continue;
+            }
+                while (*i->begin() != '[')
+                    {
+                        if (i->find("=") != std::string::npos)
+                            {
+                                val_name = i->substr(0, i->find("="));
+                                remove_spaces(val_name);
+                                val = i->substr(i->find('=') + 1, (i->find(';') - i->find('=') - 1));
+                                val = remove_spaces(val);
+                                section_list[index].set_value(val_name, val);
+                                break;
+                            }
+                        else
+                            break;
+                    }
+        }
+    }       
 
     IniParser(std::string filename)
     {
         file.open(filename);
-        auto i = table.begin();
         int str_num{};
-        std::string str;
-        
+        std::string str;  
         while (!file.eof())
             {
                 getline(file, str);
-                auto j = str.begin();
-                auto z = str.end();
-                table.insert(table.end(), std::pair<int, std::string>(++str_num, str));
-                
-                if ((*str.begin() == '[') && (*str.end() == ']'))
-                    {
-                        Section section(str);
-                        str.erase(j);
-                        str.erase(z);
-                        section_list.push_back(section);
-                        do
-                        {
-                            getline(file,str);
-                                if (str.find("=") != std::string::npos)
-                                {
-                                    section.set_value(str.substr(0, str.find("=")), str.substr(str.find("=") + 1, str.end() - str.find("=")));
-
-                                }
-
-                        }while (*str.begin() != '[');
-                    }
-                
-                str.clear();
-
+                if (!str.empty())
+                    table.push_back(str);
             }
-    };
+        file.close();
+    }
 
-    void print ()
+    void print()
     {
         for (auto i : section_list)
         {
@@ -87,6 +170,8 @@ class IniParser
 int main()
 {
     IniParser parser("file.ini");
+    parser.set_section();
+    parser.get_value("section1.val1");
     parser.print();
 
    return 0;
